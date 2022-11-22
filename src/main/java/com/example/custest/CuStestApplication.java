7c36443c9f63e818a4005d273f6689997c984915
Yaroslav Shkvarla <slav.shkvarlo@gmail.com>
2022-11-22 11:31:56 +0200

    second commit

---
+++ src/main/java/com/example/custest/CuStestApplication.java
@@ -0,0 +1,13 @@
+package com.example.custest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuStestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CuStestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/custest/CuStestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.custest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuStestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
